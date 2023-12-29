6a68a9349fe187ae2b220ecb30dc16ea3b8e8127
Grant <grantdwoods@outlook.com>
2023-12-29 13:36:52 -0800

    Dump azure template for next js boilerplate

---
+++ deck-manager/src/app/route2/page.tsx
@@ -0,0 +1,8 @@
+//routing between components is by folder convention
+//any directory under the app folder will create a route using the directory name (sub-directories will make nested routes)
+
+export default function Route2() {
+  return (
+    <div>Route 2</div>
+  )
+}
